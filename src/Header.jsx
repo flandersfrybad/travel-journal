952af336d0914426ae47ffb59ccb03dc06556edd
flandersfrybad <152252386+flandersfrybad@users.noreply.github.com>
2025-03-15 00:03:45 -0600

    Update Header.jsx

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,4 +1,4 @@
-import globePath from "https://github.com/flandersfrybad/travel-journal/blob/main/dist/images/globe.png?raw=true"
+import globePath from "http://github.com/flandersfrybad/travel-journal/blob/main/dist/images/globe.png"
 
 export default function Header() {
     return (
