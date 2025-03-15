110cbe6c41ffdf698af77a634116d25300929b06
flandersfrybad <152252386+flandersfrybad@users.noreply.github.com>
2025-03-14 20:45:50 -0600

    back

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,12 +1,9 @@
 import globe from '../images/globe.png';
 
 export default function Header() {
-    // Ensure that you use the correct path with VITE prefix
-    const globePath = import.meta.env.VITE_PUBLIC_URL + '/images/globe.png';
-    
     return (
         <header>
-            <img src={globePath} alt="globe icon" />
+            <img src={globe} alt="globe icon" />
             <h1>my travel journal.</h1>
         </header>
     );
