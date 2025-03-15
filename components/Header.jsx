cfd95fd811dcf74595efd2a4378eb8f3e2527966
flandersfrybad <152252386+flandersfrybad@users.noreply.github.com>
2025-03-14 22:44:34 -0600

    Update Header.jsx

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,7 +1,7 @@
 export default function Header() {
     return (
         <header>
-            <img src="./globe.PNG" alt="globe icon" />
+            <img src="/globe.PNG" alt="globe icon" />
             <h1>my travel journal.</h1>
         </header>
     );
