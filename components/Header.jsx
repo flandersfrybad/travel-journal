c4852c50f4b4a439f8907d7375275e08d6e9e2ce
flandersfrybad <152252386+flandersfrybad@users.noreply.github.com>
2025-03-14 20:40:15 -0600

    Update Header.jsx
    VITE_PUBLIC_URL

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,8 +1,13 @@
+import globe from '../images/globe.png';
+
 export default function Header() {
+    // Ensure that you use the correct path with VITE prefix
+    const globePath = import.meta.env.VITE_PUBLIC_URL + '/images/globe.png';
+    
     return (
         <header>
-            <img src= ".../images/globe.png?raw=true" alt="globe icon" />
+            <img src={globePath} alt="globe icon" />
             <h1>my travel journal.</h1>
         </header>
-    )
+    );
 }
