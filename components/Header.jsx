0a1f2c867575c3ccfbf69d85d2741965c0d1f328
flandersfrybad <152252386+flandersfrybad@users.noreply.github.com>
2025-03-14 20:33:19 -0600

    Update Header.jsx
    straight

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,9 +1,7 @@
-import globe from "https://github.com/flandersfrybad/travel-journal/blob/main/images/globe.png?raw=true"
-
 export default function Header() {
     return (
         <header>
-            <img src={globe} alt="globe icon" />
+            <img src= "https://github.com/flandersfrybad/travel-journal/blob/main/images/globe.png?raw=true" alt="globe icon" />
             <h1>my travel journal.</h1>
         </header>
     )
